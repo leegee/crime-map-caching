e6dd277500beedd7c707578a1324b1293ef1599a
Lee Goddard <leegee@gmail.com>
2025-08-29 15:46:27 +0200

    Base

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import packageJson from './package.json';
 
 export default defineConfig({
   plugins: [solid()],
+  base: `/${packageJson.name}}/`,
 })
